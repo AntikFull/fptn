297cdbd6c90673d3aaea0ca083ed6048f1faf48c
AntikFull <antikfull@users.noreply.github.com>
2026-06-21 12:50:20 +0700

    Patch CamouflageTLS to disable Werror on mipsel

--- depends/cmake/CamouflageTLS.cmake
+++ depends/cmake/CamouflageTLS.cmake
@@ -1,7 +1,18 @@
 include(FetchContent)
 
 FetchContent_Declare(CamouflageTLS URL https://github.com/fptn-project/camouflage-tls/archive/refs/heads/main.zip)
-FetchContent_MakeAvailable(CamouflageTLS)
+
+FetchContent_GetProperties(CamouflageTLS)
+if(NOT camouflagetls_POPULATED)
+  FetchContent_Populate(CamouflageTLS)
+  # Remove -Werror and set warning as error to OFF
+  file(READ "${camouflagetls_SOURCE_DIR}/CMakeLists.txt" CM_CONTENT)
+  string(REPLACE "-Werror" "" CM_CONTENT "${CM_CONTENT}")
+  string(REPLACE "set(CMAKE_COMPILE_WARNING_AS_ERROR ON)" "set(CMAKE_COMPILE_WARNING_AS_ERROR OFF)" CM_CONTENT "${CM_CONTENT}")
+  file(WRITE "${camouflagetls_SOURCE_DIR}/CMakeLists.txt" "${CM_CONTENT}")
+  
+  add_subdirectory(${camouflagetls_SOURCE_DIR} ${camouflagetls_BINARY_DIR})
+endif()
 
 set(CamouflageTLS_INCLUDE_DIR "${camouflagetls_SOURCE_DIR}/include")
 set(CamouflageTLS_INCLUDE_DIRS ${CamouflageTLS_INCLUDE_DIR} CACHE PATH "Camouflage Include Directories")
